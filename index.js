94ee5657488a33bbc9765638a1f4fb4010fe3814
Sarah Rollins <s.rollins751@gmail.com>
2023-04-06 07:34:41 +0900

    set up initial portfolio site files

---
+++ index.js
@@ -0,0 +1,15 @@
+(async () => {
+  await tsParticles.load("tsparticles", {
+    particles: {
+      color: {
+        value: "#c188cf",
+      }
+    },
+    background: {
+      color: {
+        value: "#191919",
+      }
+    },
+    preset: "firefly",
+  });
+})();
