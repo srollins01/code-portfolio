0db441fb15220a7b61290ee152b49e32efb99852
Sarah Rollins <s.rollins751@gmail.com>
2023-04-06 11:50:23 +0900

    added scroll to projects section, edited about section columns

--- index.js
+++ index.js
@@ -13,3 +13,12 @@
     preset: "firefly",
   });
 })();
+
+
+const seeProjects = () => {
+  const projects = document.getElementById("projects");
+  return projects.scrollIntoView();
+};
+
+const viewProj = document.getElementById("view-proj");
+viewProj.addEventListener("click", seeProjects);
