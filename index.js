07f1dc03abbff41bf6d0fb68d6d888d870c3e086
Sarah Rollins <s.rollins751@gmail.com>
2024-06-15 01:52:06 +0900

    Added scroll to top button

--- index.js
+++ index.js
@@ -23,3 +23,26 @@ const seeProjects = () => {
 
 const viewProj = document.getElementById("view-proj");
 viewProj.addEventListener("click", seeProjects);
+
+
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+const isActive = () => mybutton.classList.contains("active");
+
+function scrollFunction() {
+  if (document.body.scrollTop > (window.innerHeight - 80) || document.documentElement.scrollTop > (window.innerHeight - 80)) {
+    mybutton.classList.add("active");
+  } else {
+    if (isActive()) {
+      mybutton.classList.remove("active");
+    }
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
